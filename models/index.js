88c02d01a18648aa7eec5a7bbccb8a3b6caf15e7
RogelioSan09 <rogeliosnchz@gmail.com>
2023-05-12 02:01:37 -0700

    update

--- models/index.js
+++ models/index.js
@@ -1,5 +1,4 @@
 import User from './UserModel.js';
 import UserFavorite from './UserFavoriteModel.js';
-import Recipe from './RecipeModel.js';
 
-export default { User, UserFavorite, Recipe };
+export default { User, UserFavorite };
